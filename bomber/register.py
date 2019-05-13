433926297dba595be5eed0fe55e0587b94fa1e66
Izabela <izazi.777@gmail.com>
2019-05-13 18:47:39 +0200

    dodanie pliku stylu stylów etc

--- bomber/register.py
+++ bomber/register.py
@@ -5,3 +5,12 @@ bp = Blueprint('register', __name__, url_prefix='/')
 @bp.route('/')
 def register():
 	return render_template('register/index.html')
+
+@bp.route('/games_list')
+def games_list():
+	return render_template('register/games_list.html')
+
+@bp.route('/game_view')
+def game_view():
+	return render_template('register/game_view.html')
+	
